5db922386d6878e9087122e0c9e88bef11abb3d0
TBidart <telmoptbidart@gmail.com>
2023-02-03 05:32:49 +0000

    erro 1

---
+++ pokemon/index.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d')
+canvas.width = 1024
+canvas.height = 576
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = "C:\Users\telmi\Desktop\VS\pokemon\img\mapa.png"
+console.log(image)
+
+image.onload = () => {
+  c.drawImage(image, 0, 0)
+}
